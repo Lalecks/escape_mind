c6ca877934f462e317023582eb962c0102552552
Alexandre <alexlavaud@outlook.fr>
2021-02-11 11:07:40 +0100

    La base

---
+++ resources/views/home.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Escape mind - l'évasion virtuelle</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+...
+<!-- Le reste du contenu -->
+...
+</body>
+</html>
