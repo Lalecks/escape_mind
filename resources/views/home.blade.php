69463eb29fa558e2e21b70e89179aea7ec941630
Alexandre <alexlavaud@outlook.fr>
2021-02-16 16:38:55 +0100

    ScrollBar (almost functionnel)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,21 @@
-<h1>test</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" type="image/ico" href="" />
+    <link href="{{ asset('css/slide.css') }}" rel="stylesheet">
+    <title> Escape Mind - website </title>
+</head>
+<body>
+    <nav class="navmenu">
+        <ul>
+            <li><a href="#" class="active">Accueil</a></li>
+            <li><a href="#">Histoire</a></li>
+            <li><a href="#">Classement</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">A propos</a></li>
+        </ul>
+    </nav>
+    <script src="{{ asset('css/slide.js') }}"></script>
+</body>
+</html>
