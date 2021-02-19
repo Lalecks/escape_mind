31dcad919be387a17605da479effa0261b76ecc2
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-02-19 14:43:42 +0100

    setting up of the Scss + storage of the styles

--- public/js/divers.js
+++ public/js/divers.js
@@ -3,10 +3,10 @@ $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container');
 $(document).pjax('[data-pjax-toggle] a, a[data-pjax-toggle]', '#pjax-container', { push: false});
 
 // Loader
-// setTimeout(
-//     function() 
-//     {
-//         window.location.replace("{{ $url }}");
-//     }, 3000
-//     // Rediriger l'utilisateur vers l'endroit où il veut aller après 3 secondes
-// ); 
+setTimeout(
+    function() 
+    {
+        window.location.replace("{{ $url }}");
+    }, 3000
+    // Rediriger l'utilisateur vers l'endroit où il veut aller après 3 secondes
+); 
