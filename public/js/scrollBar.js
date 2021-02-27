e8cee5aad3afd6c09bb23b927bd501ded1e382c8
Alexandre <alexlavaud@outlook.fr>
2021-02-27 11:58:54 +0100

    NavBar not done

--- public/js/scrollBar.js
+++ public/js/scrollBar.js
@@ -8,8 +8,8 @@ window.addEventListener('scroll',() => {
     let scr = window.scrollY;
     let scrolled = (scr / max_scroll).toPrecision(3);
     let nb_elem = allItems.length;
-    const home = document.getElementById("home").clientHeight;
-    const story = document.getElementById("story").clientHeight;
+    const home = document.querySelector("#home").clientHeight;
+    const story = document.querySelector("#story").clientHeight;
     const rank = document.getElementById("rank").clientHeight;
     const about = document.getElementById("about").clientHeight;
     const contact = document.getElementById("contact").clientHeight;
@@ -26,17 +26,21 @@ window.addEventListener('scroll',() => {
 
     //Modulabilité
 
-   // if (scr < home) {
-   //      barre.style.height = "calc("
-   //          + (scr / (home / (1 / (nb_elem - 1)))) * 100 + "% - "
-   //          + (scr / (home / (1 / (nb_elem - 1)))) * 19
-   //          + "% + 19%)";
-   //  }else if (scr < story) {
-   //      barre.style.height = "calc("
-   //          + (scr / (story / (1 / (nb_elem - 1)))) * 100 + "% - "
-   //          + (scr / (story / (1 / (nb_elem - 1)))) * 19
-   //          + "% + 19%)";
-   //  }else if (scr < rank) {
+   if (scr < home) {
+        barre.style.height = "calc("
+            + (scr / (home / (1 / (nb_elem - 1)))) * 100 + "% - "
+            + (scr / (home / (1 / (nb_elem - 1)))) * 19
+            + "% + 19%)";
+       allItems[1].classList.remove("active");
+    }
+   else if (scr < story) {
+    allItems[1].classList.add("active");
+        barre.style.height = "calc("
+            + (scr / (story / (1 / (nb_elem - 1)))) * 100 + "% - "
+            + (scr / (story / (1 / (nb_elem - 1)))) * 19
+            + "% + 19%)";
+    }
+   //else if (scr < rank) {
    //      barre.style.height = "calc("
    //          + (scr / (rank / (3 / (nb_elem - 1)))) * 100 + "% - "
    //          + (scr / (rank / (3 / (nb_elem - 1)))) * 19
@@ -49,6 +53,18 @@ window.addEventListener('scroll',() => {
    //  }
 
 
+    // if (scr < home) {
+    //          barre.style.height = "calc("
+    //              + (scr / (home / (1 / (nb_elem - 1)))) * 100 + "% - "
+    //              + (scr / (home / (1 / (nb_elem - 1)))) * 19
+    //              + "% + 19%)";
+    // }else
+    // if ((scr-home) < (story)) {
+    //          barre.style.height = "calc("
+    //              + ((scr-home) / (story / (1 / (nb_elem - 1)))) * 100
+    //              + "% + 14%)";
+    //          console.log("home :" + home + "\nstory :" + (story+home) + "\nscr :" + scr);
+    //  }
 
 })
 
