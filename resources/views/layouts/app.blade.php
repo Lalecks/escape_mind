81ac39fe081fb333d5c2860fa017b18795543dd4
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-03-04 13:16:26 +0100

    icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,21 +9,17 @@
 		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 		<meta name='apple-mobile-web-app-capable' content='yes' />
-
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-
 		<!-- Référencement -->
 		<title>{{ config('app.name') }}&nbsp;-&nbsp;l'escape game en ligne</title>
 		<meta name="title" content="{{ config('app.name') }}&nbsp;-&nbsp;l'escape game en ligne"/>
-
 		{{-- <link rel="icon" type="image/png" href=""/> --}}
 		<meta name="description" content="Des chercheurs sont sur le point de débrancher l’IA qu’ils ont créée quelques années auparavant, car celle-ci est devenue obsolète.">
 		<meta name="keywords" content="escape game, play, artificial intelligence, numeric"/>
 		<meta name="copyright" content="© escape_mind"/>
 		<meta name="robots" content="index, follow, archive"/>
 		<link rel='canonical' href='/'/>
-
 		<!-- OpenGraph -->
 		<meta property="og:type" content="website"/>
 		<meta property="og:site_name" content="escape_mind"/>
@@ -31,13 +27,27 @@
 		<meta property="og:title" content="{{ config('app.name') }}&nbsp;-&nbsp;l'escape game en ligne"/>
 		<meta property="og:description" content="Des chercheurs sont sur le point de débrancher l’IA qu’ils ont créée quelques années auparavant, car celle-ci est devenue obsolète."/>
 		<meta property="og:locale" content="fr_FR"/>
-
 		<!-- Icônes -->
-
+		<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('ressources/icon/apple-icon-57x57.png')}}">
+		<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('ressources/icon/apple-icon-60x60.png')}}">
+		<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('ressources/icon/apple-icon-72x72.png')}}">
+		<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('ressources/icon/apple-icon-76x76.png')}}">
+		<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('ressources/icon/apple-icon-114x114.png')}}">
+		<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('ressources/icon/apple-icon-120x120.png')}}">
+		<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('ressources/icon/apple-icon-144x144.png')}}">
+		<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('ressources/icon/apple-icon-152x152.png')}}">
+		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('ressources/icon/apple-icon-180x180.png')}}">
+		<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('ressources/icon/android-icon-192x192.png')}}">
+		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('ressources/icon/favicon-32x32.png')}}">
+		<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('ressources/icon/favicon-96x96.png')}}">
+		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('ressources/icon/favicon-16x16.png')}}">
+		<link rel="manifest" href="{{ asset('//manifest.json')}}">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
 		<!-- Stylesheets -->
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 		<link rel="prefetch" href="../game/room1.blade.php">
-
 		<!-- Scripts -->
 		<script src='https://kit.fontawesome.com/c51a60e485.js' crossorigin='anonymous' integrity='sha384-NBHAuYUNWKduo4crumSk720p46lSGmSF7SDtoMEmu+SnsanQ94l8NiUhAPI0UIqx'></script>
 	</head>
@@ -117,9 +127,9 @@
 		</div>
 		--}}
 
-		<div id="pjax-container">
+		<main id="pjax-container">
 			@yield('content')
-		</div>
+		</main>
 
 		<!--====== Javascripts & Jquery ======-->
 		<script src="{{ asset('js/app.js') }}" defer></script>
