88a605197d2759e2eef41320bdad6c6e68afdb52
Alexandre <alexlavaud@outlook.fr>
2021-03-10 23:01:17 +0100

    Essai jeu, non fonctionnel

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,6 +9,6 @@ class GameController extends Controller
     public function game(Request $request)
     {
         $url = $request['url'];
-        return view('/game/room1', compact('url'));
+        return view('/game/jeu', compact('url'));
     }
 }
