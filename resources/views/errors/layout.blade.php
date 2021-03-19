77d74648334dec5aba32e97b01c159c5d584bea7
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-03-20 00:09:19 +0100

    update error layout

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,16 +1,17 @@
 {{-- php artisan vendor:publish --tag=laravel-errors --}}
 @extends('layouts.template')
-
+@section('content')
 <div class="error">
 	<div class="code" data-text="@yield('code')">@yield('code', __('Oh no'))</div>
-	<p>Aïe ! @yield('message')</p>
-	<a class="hoverable btn btn-animation_glitch" href="{{ app('router')->has('home') ? route('home') : url('/ ') }}">{{ __('Accueil') }}</a>
-    {{-- @yield('image') --}}
+	<h2>Aïe ! @yield('message')</h2>
+	<p>Vous pouvez revenir à <a class="hoverable link_hover" href="Javascript:history.go(-1)">la page précédente</a> ou aller à <a class="hoverable link_hover" href="{{ app('router')->has('home') ? route('home') : url('/') }}">{{ __("la page d'accueil") }}</a>.</p>
+	{{-- <img src="{{ asset('ressources/404.gif') }}" alt="@yield('code')_error"> --}}
 </div>
-
-<script>
+<!--====== Javascripts & Jquery ======-->
+{{-- <script>
 	window.setTimeout(function(){
 		// Après 2 minutes, rediriger vers l'accueil
 		window.location.href = "/";
 	}, 20000);
-</script>
+</script> --}}
+@endsection
