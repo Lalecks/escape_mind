282a8d5a45904dbdb6828a72f3e4ec96c8e4041a
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-04-13 15:23:24 +0200

    hover obj

--- resources/js/game/room_creation.js
+++ resources/js/game/room_creation.js
@@ -6,17 +6,16 @@
 
 import createTimer from "./components/timer";
 
-// let objects = [
-//     ["Casettes", 1],
-//     ["Coffre_Fort", 1],
-//     ["Jukebox", 1],
-//     ["Lecteur", 1],
-//     ["Radio", 1],
-//     ["Ramdam", 1]
-// ];
-//let actualRoom = 1;
-
+// Ajout des objets
 let objects = [
+    /* Room 1 */
+    ["Casettes", 1],
+    ["Coffre_Fort", 1],
+    ["Jukebox", 1],
+    ["Lecteur", 1],
+    ["Radio", 1],
+    ["Ramdam", 1],
+    /* Room 2 */
     ["Lit", 2],
     ["Peluche", 2],
     ["Bougie", 2],
@@ -26,7 +25,10 @@ let objects = [
     ["Pile", 2],
     ["Pile", 2],
     ["Pile", 2],
+    /* Room 3 */
 ];
+
+// Salle actuelle
 let actualRoom = 2;
 
 export default function createGame() {
@@ -34,47 +36,55 @@ export default function createGame() {
 }
 
 function createRoom() {
+    // Parents du bg
     let parent = document.getElementById("Objects");
+    // Background
     let background = document.getElementById("Background");
+    background.className = "Room" + actualRoom;
     let bg = document.createElement("img");
     bg.id = "bg";
-    // bg.style="position:fixed; bottom:0px; z-index=0;";
-    //bg.src = "./resources/game/rooms/Room1.png";
-    // bg.src = "./resources/game/Chambre_tous_objets.png";
-    bg.src = "./resources/game/rooms/Room2.png";
+    bg.src = "./resources/game/rooms/Room" + actualRoom + ".png";
 
-    //INIT OBJETS DE LA SALLE ACTUEL
+    // Initialisation des objets de la salle actuelle
     for (let i = 0; i < objects.length; i++) {
         if (objects[i][1] === actualRoom) {
-            let a = document.createElement("a");
-            a.id = "a_" + objects[i][0];
-
-            // a.className = "hoverable";
-
+            let link = document.createElement("div");
+            link.id = "link_" + objects[i][0];
+            link.href = "#";
+            // link.className = "hoverable";
+            // Objet en cours
             let obj = document.createElement("img");
             obj.id = "" + objects[i][0];
             obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + objects[i][0] + "_00.png";
-            // obj.style = "position:fixed; left:" + objects[i][2] + "px; bottom: " + objects[i][3] + "px;";
-            // obj.className = "hoverable";
-            obj.href = "https://google.fr";
+            obj.className = "hoverable"; // Pour centrer la souris mdr.
 
-            a.addEventListener("mouseenter", () => {
-                try {
-                    obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + objects[i][0] + "_01.png";
-                } catch (e) {
-                    console.log(e);
-                }
+            // Effet au survol
+            // link.addEventListener("mouseenter", () => {
+            //     if (actualRoom === 1) {
+            //         obj.style = "-webkit-filter: saturate(180%);";
+            //     } else if (actualRoom === 2) {
+            //         // obj.style = "-webkit-filter: sepia(60%) drop-shadow(5px 5px 5px #222);";
+            //     }
+            // })
+            // link.addEventListener("mouseleave", () => {
+            //     obj.style = "";
+            // })
 
-            })
-            a.addEventListener("mouseleave", () => {
-                obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + objects[i][0] + "_00.png";
-            })
+            // a.addEventListener("mouseenter", () => {
+            //     try {
+            //         obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + objects[i][0] + "_01.png";
+            //     } catch (e) {
+            //         console.log(e);
+            //     }
+            // })
+            // a.addEventListener("mouseleave", () => {
+            //     obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + objects[i][0] + "_00.png";
+            // })
 
-            a.appendChild(obj);
-            parent.appendChild(a);
+            link.appendChild(obj);
+            parent.appendChild(link);
         }
     }
-
     background.appendChild(bg);
 }
 
--- resources/views/game/jeu.blade.php
+++ resources/views/game/jeu.blade.php
@@ -18,15 +18,50 @@
     </div>
     <div id="Game" class="Game">
         <div id="Background">
-        
-        <div id="Objects"></div>
+            <div id="Objects"></div>
         </div>
-        
     </div>
     <div id="Interface" class="Interface">
         <!--Timer-->
     </div>
-
 </main>
-{{--  --}}
+
+
+
+{{--
+<div class="grid">
+            
+            <div class="filterDiv show lien-cliquable web" data-id='m18'>
+                <img alt="meetgeek" src="Ressources/real-pro/meetgeek.png">
+                <div class="real-over">
+                    <p class="tittle-real"><strong>MeetGeek</strong></p>
+                    <p class="support">Web</p>
+                </div>
+            </div>
+</div>
+ <div id="real-caché" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <div id='content'></div>
+            </div>
+        </div>
+
+        <div class="cache">
+
+            <div class='hidden' id="m18">
+                <h1><strong>MeetGeek</strong></h1>
+                <div>
+                    <div class="production-realisation">
+                        <img alt="meetgeek" src="Ressources/real-pro/meetgeek.png">
+                    </div>
+                    <div class="description-realisation">L’objectif était de créer et réaliser un mini facebook. En binôme, je me suis occupée de la partie web (front et back), tandis que ma coéquipière a confectionné les maquettes du site.
+                        <div>
+                            <br>
+                            <a href="https://meetgeek.000webhostapp.com/index.php" target="_blank" class="boutton">MeetGeek</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
 @endsection
