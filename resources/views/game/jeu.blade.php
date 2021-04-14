e3bdca6d1e4a8dd3de62189cb3718da034e8b924
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-04-14 19:47:34 +0200

    css anim

--- resources/views/game/jeu.blade.php
+++ resources/views/game/jeu.blade.php
@@ -2,30 +2,30 @@
 @section('title', __("le jeu"))
 
 @section('content')
-
-<div id="game-page" class="landscape">
-{{-- <p>zse</p> --}}
-<header id="Header" class="Header">
-    <div class="HeaderLogo">
-    </div>
-    <div class="Mute">
-    </div>
-</header>
-
-<main id="Main" role="main">
-    <section id="Cinematic" class="Cinematic">
-        <!-- Cinematic -->
-    </section>
-    <section id="Game" class="Game">
-        <!--Jeu -->
-        <div id="Area">
-            <!-- Aire de jeu -->
-            <div id="Objects"></div>
-            <div id="Decors"></div>
+<section id="game-page" class="landscape">
+    {{-- <p>zse</p> --}}
+    <header id="Header" class="Header">
+        <div class="HeaderLogo">
         </div>
-    </section>
-    <section id="Interface" class="Interface">
-        <!-- Timer -->
-    </section>
-</main>
+        <div class="Mute">
+        </div>
+    </header>
+
+    <main id="Main" role="main">
+        <div id="Cinematic" class="Cinematic">
+            <!-- Cinematic -->
+        </div>
+        <div id="Game" class="Game">
+            <!--Jeu -->
+            <div id="Area">
+                <!-- Aire de jeu -->
+                <div id="Objects"></div>
+                <div id="Decors"></div>
+            </div>
+        </div>
+        <div id="Interface" class="Interface">
+            <!-- Timer -->
+        </div>
+    </main>
+</section>
 @endsection
