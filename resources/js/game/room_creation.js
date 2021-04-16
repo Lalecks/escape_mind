cd0fad3f117a3fbf5aa0a3d392dd107a8e37d7f5
Alexandre <alexlavaud@outlook.fr>
2021-04-16 16:29:19 +0200

    Modal boxes!!! (sans activités, et css bof)

--- resources/js/game/room_creation.js
+++ resources/js/game/room_creation.js
@@ -50,36 +50,36 @@ export default function createGame() {
 }
 
 function createRoom() {
+    let url = "./resources/game/room" + actualRoom;
     let parent_obj = document.getElementById("Objects");
     let parent_decor = document.getElementById("Decors");
+
     // area
     let area = document.getElementById("Area");
     area.className = "Room" + actualRoom;
+
     let bg = document.createElement("img");
     bg.id = "bg";
-    bg.src = "./resources/game/rooms/Room" + actualRoom + ".png";
+    bg.src = url + "/Room" + actualRoom + ".png";
 
     // Initialisation des objets de la salle actuelle
     for (let i = 0; i < object.length; i++) {
         if (object[i][1] === actualRoom) {
             let link = document.createElement("div");
             link.id = "link_" + object[i][0];
-            //On appelle la classe juste par notation
+            // On appelle la classe juste par notation
             link.className = "trigger";
 
             //Pour les triggers ajoutés en JS
-            link.addEventListener("click", function(){
-                toggleModalCustom(object[i][0],object[i][2],
-                    "./resources/game/objects/image/room" + actualRoom + "/" + object[i][0] + "_01.png")
+            link.addEventListener("click", function () {
+                toggleModalCustom(object[i][0], object[i][2],
+                    url + "/objects/" + object[i][0] + "_01.png")
             });
-
-
-
 
             // Objet en cours
             let obj = document.createElement("img");
             obj.id = "" + object[i][0];
-            obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + object[i][0] + "_00.png";
+            obj.src = url + "/objects/" + object[i][0] + "_00.png";
             obj.className = "hoverable"; // Pour centrer la souris mdr.
 
             link.appendChild(obj);
@@ -94,9 +94,7 @@ function createRoom() {
 
             let obj = document.createElement("img");
             obj.id = "" + decor[i][0];
-            obj.src = "./resources/game/objects/image/room" + actualRoom + "/" + decor[i][0] + "_00.png";
-
-
+            obj.src = url + "/decors/" + decor[i][0] + "_00.png";
             link.appendChild(obj);
             parent_decor.appendChild(link);
         }
@@ -104,10 +102,10 @@ function createRoom() {
     }
     area.appendChild(bg);
 
-    //pour sortir de la modale en cliquant dans le vide
-    window.addEventListener("click",function(event){
+    // Pour sortir de la modale en cliquant dans le vide
+    window.addEventListener("click", function (event) {
         if (event.target === document.querySelector(".modal")) {
-            toggleModalCustom("","","");
+            toggleModalCustom("", "", "");
         }
     })
 }
