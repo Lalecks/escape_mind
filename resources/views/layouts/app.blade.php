67da90ba9360e2510f848b2948a36d19e8dedd41
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-05-01 23:28:17 +0200

    test mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,83 +64,24 @@
 	</head>
 	<!--====== Corps ======-->
 	<body>
+		<!-- Loader -->
+		<div id="overlay"> 
+			<div id="progstat"></div>
+			<div id="progress"></div>
+		</div>
 		<!-- Souris -->
 		<div id="souris" style="opacity: 0">
 			<div class="cursor cursor--small"></div>
 			<canvas class="cursor cursor--canvas"></canvas>
 		</div>
 
-		{{--
-		<div id="app">
-			<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-				<div class="container">
-					<a class="navbar-brand" href="{{ url('/') }}">
-						{{ config('app.name', 'Laravel') }}
-					</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<!-- Left Side Of Navbar -->
-						<ul class="navbar-nav mr-auto">
-
-						</ul>
-
-						<!-- Right Side Of Navbar -->
-						<ul class="navbar-nav ml-auto">
-							<!-- Authentication Links -->
-							@guest
-							@if (Route::has('login'))
-							<li class="nav-item">
-								<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-							</li>
-							@endif
-
-							@if (Route::has('register'))
-							<li class="nav-item">
-								<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-							</li>
-							@endif
-							@else
-							<li class="nav-item dropdown">
-								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									{{ Auth::user()->name }}
-								</a>
-
-								<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-									<a class="dropdown-item" href="{{ route('logout') }}"
-									   onclick="event.preventDefault();
-												document.getElementById('logout-form').submit();">
-										{{ __('Logout') }}
-									</a>
-
-									<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-										@csrf
-									</form>
-								</div>
-							</li>
-							@endguest
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</div>
-		--}}
-
 		<main>
-			<div id="overlay"> <!--loader-->
-				<div id="progstat"></div>
-				<div id="progress"></div>
-			</div>
-            <div id="fullscreen" style="opacity:0;">
-
-            </div>
+            <div id="fullscreen" style="opacity:0;"></div>
 			<div id="content" style="opacity: 0">
 				@yield('content')
 			</div>
 		</main>
-
+		
 		<!--====== Javascripts & Jquery ======-->
 		<script src="https://cdn.jsdelivr.net/gh/dixonandmoe/rellax@master/rellax.min.js"></script>
 		<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js'></script>
