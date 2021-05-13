f6d855c661df7e6e8fad72649d666b33d4db0d79
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-05-14 01:42:29 +0200

    settings design

--- resources/js/game/components/settings.js
+++ resources/js/game/components/settings.js
@@ -5,31 +5,31 @@
  * */
 
 export default function settings() {
-    /* Affichage/Cacher settings-menu  des settings */
+    /* Affichage/Cacher settings-menu des settings */
     $('.burger').click(function () {
         // au click du bouton ayant la class burger :
         $(this).toggleClass('burger-open');
         // on ajoute ou enlève la class burger-open au bouton.burger
-        $('.settings-menu ').toggleClass('is-open');
-        // on ajoute ou enlève la class is-open à la nav.settings-menu 
+        $('.settings-menu').toggleClass('is-open');
+        // on ajoute ou enlève la class is-open à la nav.settings-menu
     });
-    $('.settings-menu  ul li a').click(function () {
+    $('.settings-menu ul li a').click(function () {
         // au click d'un des a :
-        $('.settings-menu ').removeClass('is-open');
-        // on enlève la class is-open à la nav.settings-menu 
+        $('.settings-menu').removeClass('is-open');
+        // on enlève la class is-open à la nav.settings-menu
         $('.burger').removeClass('burger-open');
         // on enlève la class burger-open au bouton.burger
         // utile pour un one-page, car on ne veut pas que la nav reste ouverte une fois que la page est arrivée au bon endroit
     });
 
-    /* Fermer le settings-menu  des paramètres si vous cliquez en dehors de la DIV */
-    let settings  = document.querySelector('.settings-menu ')
+    /* Fermer le settings-menu des paramètres si vous cliquez en dehors de la DIV */
+    let menu = document.querySelector('.settings-menu')
     let engrenage = document.querySelector(".fas");
     let game = document.getElementById("Game");
 
     window.addEventListener('click', function (e) {
         if (document.querySelector(".is-open") && e.target === game) {
-            settings .removeClass('is-open');
+            menu.removeClass('is-open');
             $('.burger').removeClass('burger-open');
         }
     })
--- resources/views/game/settings.blade.php
+++ resources/views/game/settings.blade.php
@@ -1,55 +1,55 @@
 <menu id="settings-panel">
-    <span class="burger hoverable fas fa-cog"></span>
-    <nav class="settings-menu glass-effect">			
-        <div id="filter">
-            <span class="tablinks active" onclick="openSetting(event,'Story')"><i class="fas fa-book-open"></i></span>
-            <span class="tablinks" onclick="openSetting(event,'Sound')"><i class="fas fa-music"></i></span>
-            <span class="tablinks" onclick="openSetting(event,'Languages')"><i class="fas fa-globe-europe"></i></span>
-            <span class="tablinks" onclick="openSetting(event,'Info')"><i class="fas fa-info"></i></span>
-            <span class="tablinks" onclick="openSetting(event,'Deconnexion')"><i class="fas fa-sign-out-alt"></i></span>
-        </div>
+	<div class="burger hoverable fas fa-cog"></div>
+	<nav class="settings-menu glass-effect">			
+		<div id="filter">
+			<span class="tablinks active" onclick="openSetting(event,'Story')"><i class="fas fa-book-open"></i></span>
+			<span class="tablinks" onclick="openSetting(event,'Sound')"><i class="fas fa-music"></i></span>
+			{{-- <span class="tablinks" onclick="openSetting(event,'Languages')"><i class="fas fa-globe-europe"></i></span> --}}
+			<span class="tablinks" onclick="openSetting(event,'Info')"><i class="fas fa-info"></i></span>
+			{{-- <span class="tablinks" onclick="openSetting(event,'Deconnexion')"><i class="fas fa-sign-out-alt"></i></span> --}}
+		</div>
 
-        <div id="tabs" class="glass-effect">
-            <div id="Story" class="tabcontent" style="display: block;">
-                <h2>Histoire</h2>
-            </div>
+		<div id="tabs" class="glass-effect">
+			<div id="Story" class="tabcontent" style="display: block;">
+				<h2>Histoire</h2>
+			</div>
 
-            <div id="Sound" class="tabcontent">
-                <h2>Son</h2>
-                <p>Volume des musiques - Sound en %</p>
-                <input class="slider glass-effect" type="range" min="1" max="100" value="50">
-                <p>Volume des musiques - Music</p>
-                <input class="slider glass-effect" type="range" min="1" max="100" value="50">
+			<div id="Sound" class="tabcontent">
+				<h2>Son</h2>
+				<div>
+					<p>Musiques</p>
+					<input class="slider glass-effect hoverable" type="range" min="1" max="100" value="50">
+				</div>
+				<div>
+					<p>Bruitages & effets sonores</p>
+					<input class="slider glass-effect hoverable" type="range" min="1" max="100" value="50">
+				</div>
+				<div>
+					<p>Maître du jeu</p>
+					<input class="slider glass-effect hoverable" type="range" min="1" max="100" value="50">
+				</div>
+			</div>
 
-                <p>Volume des bruitages/effets sonores - Sound fx</p>
-                <input class="slider glass-effect" type="range" min="1" max="100" value="50">
+			{{-- <div id="Languages" class="tabcontent">
+				<h2>Langues</h2>
+				<div>
+					<i class='fas fa-flag-usa'></i>
+				</div>
+			</div> --}}
 
-                <p>Volume du gamemaster</p>
-                <input class="slider glass-effect" type="range" min="1" max="100" value="50">
+			<div id="Info" class="tabcontent">
+				<h2>Information</h2>
+				<a href="mailto:contact.escapemind@gmail.com" class="link hoverable"><i class="fas fa-check-double"></i> Un problème ? Contactez-nous sur contact.escapemind@gmail.com</a>
+                
+				<a href="{{asset('/legal_notice')}}" class="link hoverable"><i class="fas fa-bug"></i> Informations légales</a>
 
-                <p>Notif</p>
-                <input class="slider glass-effect" type="range" min="1" max="100" value="50">
-            </div>
+                <a  href="{{asset('/')}}" class="link hoverable"><i class="fas fa-sign-out-alt"></i> Quitter le jeu</a>
+			</div>
 
-            <div id="Languages" class="tabcontent">
-                <h2>Langues</h2>
-                <div>
-                    <i class='fas fa-flag-usa'></i>
-                </div>
-            </div>
-
-            <div id="Info" class="tabcontent">
-                <h2>Information</h2>
-                <p>Un problème ?</p>
-                <p>Private policy</p>
-                <p></p>
-                <p><a href="{{asset('/legal_notice')}}">Informations légales</a></p>
-            </div>
-
-            <div id="Deconnexion" class="tabcontent">
-                <h2>Deconnexion</h2>
-                <a href="/">Quitter le jeu</a>
-            </div>
-        </div>
-    </nav>
+			{{-- <div id="Deconnexion" class="tabcontent">
+				<h2>Déconnexion</h2>
+				<a  href="{{asset('/')}}" class="link hoverable">Quitter le jeu</a>
+			</div> --}}
+		</div>
+	</nav>
 </menu>
