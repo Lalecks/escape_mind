d3edf3f475fd3983f196648a14ed0261bb2e6275
sarah-mrcrt <mauriaucourt.sarah@gmail.com>
2021-05-21 23:07:02 +0200

    new favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,23 +40,27 @@
 		<meta name="twitter:description" content="Des chercheurs sont sur le point de débrancher l’IA qu’ils ont créée quelques années auparavant, car celle-ci est devenue obsolète.">
 		<meta name="twitter:image" content="{{ asset('resources/icon/apple-icon-57x57.png')}}">
 		<!-- Icônes -->
-		<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('resources/icon/apple-icon-57x57.png')}}">
-		<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('resources/icon/apple-icon-60x60.png')}}">
-		<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('resources/icon/apple-icon-72x72.png')}}">
-		<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('resources/icon/apple-icon-76x76.png')}}">
-		<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('resources/icon/apple-icon-114x114.png')}}">
-		<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('resources/icon/apple-icon-120x120.png')}}">
-		<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('resources/icon/apple-icon-144x144.png')}}">
-		<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('resources/icon/apple-icon-152x152.png')}}">
-		<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('resources/icon/apple-icon-180x180.png')}}">
-		<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('resources/icon/android-icon-192x192.png')}}">
-		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('resources/icon/favicon-32x32.png')}}">
-		<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('resources/icon/favicon-96x96.png')}}">
-		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('resources/icon/favicon-16x16.png')}}">
-		<link rel="manifest" href="{{ asset('resources/icon/manifest.json')}}">
-		<meta name="msapplication-TileColor" content="#1A1A1A">
-		<meta name="msapplication-TileImage" content="{{ asset('resources/icon/ms-icon-144x144.png')}}">
-		<meta name="theme-color" content="#1A1A1A">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('apple-touch-icon-57x57.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('apple-touch-icon-114x114.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('apple-touch-icon-72x72.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('apple-touch-icon-144x144.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('apple-touch-icon-60x60.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('apple-touch-icon-120x120.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('apple-touch-icon-76x76.png')}}" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('apple-touch-icon-152x152.png')}}" />
+		<link rel="icon" type="image/png" href="{{ asset('resources/icon/favicon-196x196.png')}}" sizes="196x196" />
+		<link rel="icon" type="image/png" href="{{ asset('resources/icon/favicon-96x96.png')}}" sizes="96x96" />
+		<link rel="icon" type="image/png" href="{{ asset('resources/icon/favicon-32x32.png')}}" sizes="32x32" />
+		<link rel="icon" type="image/png" href="{{ asset('resources/icon/favicon-16x16.png')}}" sizes="16x16" />
+		<link rel="icon" type="image/png" href="{{ asset('resources/icon/favicon-128.png')}}" sizes="128x128" />
+		<meta name="application-name" content="&nbsp;"/>
+		<meta name="msapplication-TileColor" content="#FFFFFF" />
+		<meta name="msapplication-TileImage" content="{{ asset('resources/icon/mstile-144x144.png')}}" />
+		<meta name="msapplication-square70x70logo" content="{{ asset('resources/icon/mstile-70x70.png" ')}}">
+		<meta name="msapplication-square150x150logo" content="{{ asset('resources/icon/mstile-150x150.png')}}" />
+		<meta name="msapplication-wide310x150logo" content="{{ asset('resources/icon/mstile-310x150.png')}}" />
+		<meta name="msapplication-square310x310logo" content="{{ asset('resources/icon/mstile-310x310.png')}}" />
+		{{-- <link rel="manifest" href="{{ asset('resources/icon/manifest.json')}}"> --}}
 		<!-- Stylesheets -->
 		<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 		<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
